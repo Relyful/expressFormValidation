f36da6bf0573c3435511dc96097892af6dbeb68d
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-07 21:10:37 +0100

    First day of learning

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require("express");
+const app = express();
+const usersRouter = require("./routes/usersRouter");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
