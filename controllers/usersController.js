a4ce1fcea5d077cbfa851f74e41a04baf48eb748
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-12 20:14:17 +0100

    Allow updating for age email and bio

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -62,8 +62,8 @@ exports.usersUpdatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = req.body;
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.updateUser(req.params.id, { firstName, lastName, email, age, bio });
     res.redirect("/");
   }
 ];
